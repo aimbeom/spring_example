82587e0cf7879f221d705aa8b137467909994faf
DESKTOP-SGPRNB8\user <jbcho123@naver.com>
2022-07-01 18:49:42 +0900

    jstl_core(2) / fmt

--- src/main/java/com/example/lesson05/Lesson05Controller.java
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -1,6 +1,13 @@
 package com.example.lesson05;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,4 +21,42 @@ public class Lesson05Controller {
 	
 	//el문법 안에서 이루어지는 것들은 java에서 처리가 된디
 	//각종 연산 등은 el문법 내부에서 처리시킬것
+	
+	// http://localhost/lesson05/ex02
+		@RequestMapping("/lesson05/ex02")
+		public String ex02(Model model) {
+			//List<String>
+			List<String> fruits = new ArrayList<String>();
+			fruits.add("apple");
+			fruits.add("banana");
+			fruits.add("grape");
+			fruits.add("orange");
+			fruits.add("strawberry");
+			
+			// List<Map>
+			List<Map<String, Object>> users = new ArrayList<>();
+			Map<String, Object> user = new HashMap<>();
+			user.put("name", "조준범");
+			user.put("height", "184");
+			user.put("weight", "80");
+			users.add(user);
+			
+			user = new HashMap<>();
+			user.put("name", "김유빈");
+			user.put("height", "165");
+			user.put("weight", "53");
+			users.add(user);
+			
+			model.addAttribute("fruits", fruits);
+			model.addAttribute("users", users);
+			
+			return "lesson05/ex02";
+		}
+	// http://localhost/lesson05/ex03
+		@RequestMapping("/lesson05/ex03")
+		public String ex03(Model model) {
+			Date today = new Date();
+			model.addAttribute("today", today);
+			return "lesson05/ex03";
+		}
 }
